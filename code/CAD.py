0e48d988f1c48da3e55dfe25b1399da391f6bd7a
ishanu <ishanu.chattopadhyay@gmail.com>
2019-04-01 11:33:39 -0500

    CAD implementation added

---
+++ code/CAD.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import ccx as cx
+import pickle
+import pandas as pd
+import numpy as np
+
+
+nfeatures=211
+
+def ask(item,B=0,E=5):
+    if item < 0:
+        return {item:0}
+    response=input(' Response to item '+str(item)+': ')
+    return {item:int(response)}
+
+
+modelset=cx.load('../model/model_2_3.pkl')
+model=cx.chooseForm(modelset)
+
+
+[r,p]=cx.runCAD(model,nfeatures,ask=ask)
+print(p)
